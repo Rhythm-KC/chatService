c36c3973ea90de296c8407f7f8ca233e75348fed
Rhythm-KC <rhythmkc7@gmail.com>
2025-01-04 16:21:45 -0500

    ServerProtocal can now send message, Made changes to the internal structure as it holds a buffer

--- protocol/serverProtocol.go
+++ protocol/serverProtocol.go
@@ -2,40 +2,52 @@ package protocol
 
 import (
 	"net"
+	"time"
 
 	pe "github.com/rhythm/chatservice/protocol/error"
 	protocol "github.com/rhythm/chatservice/protocol/util"
 )
 
-
 type ServerConnection struct{
     client net.Conn
+    connectionBuffer []byte
 }
+
 
 func NewServerConnection(conn *net.Conn) *ServerConnection{
 
-    return &ServerConnection{*conn}
+    return &ServerConnection{*conn, make([]byte, 1024)}
+
 }
 
 func (conn *ServerConnection)Close(){
     conn.client.Close()
 }
 
-func (conn *ServerConnection) Listen() ([]byte, *pe.ProtocolError){
+func (conn *ServerConnection) Listen(timeout uint) (Message, *pe.ProtocolError){
 
-    var buf = make([]byte, 1024) 
-    
-    n, err := conn.client.Read(buf)
+
+    conn.client.SetReadDeadline(time.
+                                Now().
+                                Add(time.Duration(timeout) * time.Second)) 
+
+    n, err := conn.client.Read(conn.connectionBuffer)
     if err != nil{
         return nil, pe.TimedOutError("Reading from the server timedout")
     }
 
-    er := protocol.CheckForValidHeader(buf[0], buf[1:n])
+    er := protocol.CheckForValidHeader(conn.connectionBuffer[0], 
+                                       conn.connectionBuffer[1:])
     if er != nil{
         return nil, er
     }
-    return buf[:n], nil
+    return Unmarshal(conn.connectionBuffer[:n])
 
+}
+
+func (conn *ServerConnection) SendResponse(msg Message){
+    data, _:= msg.Marshal()
+    conn.client.Write(data)
 }
 
 
